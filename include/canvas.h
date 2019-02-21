55f5892b7a350436439ea5c8dd66056502bf81b2
gabrielArSouza <gabriel_feg@hotmail.com>
2019-02-20 21:25:25 -0300

    initiate a description of Canvas class

--- include/canvas.h
+++ include/canvas.h
@@ -1,15 +1,20 @@
 #ifndef _CANVAS_H_
 #define _CANVAS_H_
 
+#include "color.h"
+
+typedef unsigned int size_t;
+
 class Canvas {
 
 private:
 	
-	unsigned int width;
+	size_t m_width;
 	
-	unsigned int heigth;
+	size_t m_heigth;
 
-	
+	Color m_bkg_color;
+
 };
 
 #endif
