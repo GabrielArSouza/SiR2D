3ce2ed6813a6341af4c1a7f3b7f3c6babbd3ce2d
Gabriel Araujo De Souza <gabriel_2016@A307-10L.imd.local>
2019-02-26 07:26:43 -0300

    bug found

--- src/main.cpp
+++ src/main.cpp
@@ -17,9 +17,14 @@ int main ()
 	Point p1 = Point (10, 10);
 	Point p2 = Point (189, 189);
 
+	Point p3 = Point(10,189);
+	Point p4 = Point(189,10);
+
 	Line line = Line(&p1, &p2, &color, &color);
+	Line line2 = Line (&p3, &p4, &color, &color);
 
 	canvas.draw(&line, &color);
+	canvas.draw(&line2, &color);
 
 	Raster raster = Raster (&canvas, filename);
 	raster.draw();
