1c42ca4ec0c40c484cc3355221965a2089d143d0
Gabriel Araujo De Souza <gabriel_2016@A307-10L.imd.local>
2019-02-28 07:10:07 -0300

    resolve bug

--- src/main.cpp
+++ src/main.cpp
@@ -13,14 +13,13 @@ int main ()
 
 	Canvas canvas = Canvas(200, 200);
 	
-	Color color = Color(4,255,4);
-	
+
 	// create a line
-	// POINT p1 = {20, 20};
-	// POINT p2 = {20, 180};
-	// POINT p3 = {180, 20};
-	// POINT p4 = {180, 180};
-	// POINT p5 = {100, 100};
+	POINT p1 = {20, 20};
+	POINT p2 = {20, 180};
+	POINT p3 = {180, 20};
+	POINT p4 = {180, 180};
+	POINT p5 = {100, 100};
 
 	// Line line1 = Line(&p1, &p2, BLACK);
 	// canvas.add_new_shape(&line1);
@@ -40,13 +39,16 @@ int main ()
 	// Line line6 = Line (&p2, &p4, &color, &color);
 	// canvas.add_new_shape(&line6);
 
-	// Line line7 = Line (&p3, &p4, &color, &color);
-	// canvas.add_new_shape(&line7);
+	Line line7 = Line (&p3, &p4, PINK, PINK);
+	canvas.add_new_shape(&line7);
 
 	Circle circle = Circle (POINT {100,100}, 80, BLACK);
 	canvas.add_new_shape(&circle);
+
 	Triangle trig = Triangle(POINT{20,20}, POINT{140,60}, POINT{140,140}, BLUE);
 	canvas.add_new_shape(&trig);
+
+
 	
 
 	canvas.draw();
