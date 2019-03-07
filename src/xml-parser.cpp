6320af205848c97c3841cf13b23a30b20ec322e2
Gabriel Araujo De Souza <gabriel_2016@A307-09L.imd.local>
2019-03-07 08:11:35 -0300

    try read a xml archive

--- src/xml-parser.cpp
+++ src/xml-parser.cpp
@@ -7,11 +7,34 @@ void parser_xml (std::string filename)
     XMLDocument doc;
     doc.LoadFile( filename.c_str() );
 
-    XMLElement* titleElement = doc.FirstChildElement( "PLAY" )->FirstChildElement( "TITLE" );
-	std::string title = titleElement->GetText();
-	std::cout << "Name of play (1): " << title << std::endl;
+	XMLElement *pRoot = doc.FirstChildElement("scene")->FirstChildElement("canvas");
+	if (pRoot != NULL)
+	{
+		std::cout << "height: " << pRoot->Attribute("height") 
+			<< ", width: " << pRoot->Attribute("width") << std::endl;
 
-	XMLText* textNode = titleElement->FirstChild()->ToText();
-	title = textNode->Value();
-	std::cout << "Name of play (2): " << title << std::endl;
+		while (pRoot != NULL)
+		{
+			// Get next child
+			pRoot = pRoot->NextSiblingElement( "shape" );
+			std::string type = pRoot->Attribute("type");
+			// Is a circle?
+			if (type.compare("circle"))
+				std::cout << "this is a circle\n";
+
+		}
+	}
+	
+	// pParm = pRoot->FirstChildElement("circle");
+	// std::cout << "cx:" << pParm->Attribute("cx") << ", cy: " << pParm->Attribute("cy") 
+	// 			<< ", r: " << pParm->Attribute("r") << ", stroke: " << pParm->Attribute("stroke")
+	// 			<< ", fill: " << pParm->Attribute("fill");
+	
+    // XMLElement* titleElement = doc.FirstChildElement( "PLAY" )->FirstChildElement( "TITLE" );
+	// std::string title = titleElement->GetText();
+	// std::cout << "Name of play (1): " << title << std::endl;
+
+	// XMLText* textNode = titleElement->FirstChild()->ToText();
+	// title = textNode->Value();
+	// std::cout << "Name of play (2): " << title << std::endl;
 }
